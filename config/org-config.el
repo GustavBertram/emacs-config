d49e82885caf600aecb505eab15d08ea93df3fee
gustavbertram <gustav.bertram@gmail.com>
2019-03-26 14:09:44 +0200

    Convert org mode config to use-package

--- config/org-config.el
+++ config/org-config.el
@@ -1,8 +1,16 @@
 ;; Org mode settings
 
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq org-hide-leading-stars t)
-(setq org-odd-levels-only t)
+(use-package org
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda))
+  :config
+  (setq org-log-done t)
+  (setq org-hide-leading-stars t)
+  (setq org-odd-levels-only t)
+  (setq org-agenda-files
+	(list "~/org"
+	      "~/org/learning"
+	      "~/org/management"
+	      "~/org/meetings"
+	      "~/org/notes")))
