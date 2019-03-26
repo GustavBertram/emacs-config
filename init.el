980985731c0912cedfa435c79335410956ec2ad1
gustavbertram <gustav.bertram@gmail.com>
2019-03-26 14:26:13 +0200

    Remove custom settings from init.el

--- init.el
+++ init.el
@@ -25,24 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(column-number-mode t)
- '(custom-safe-themes
-   (quote
-    ("f10aa29e86054daf7d0a1bd9465ac41da644ccd1b70f3012e58be60c3622ed60" default)))
- '(package-selected-packages
-   (quote
-    (magit clj-refactor org-link-minor-mode htmlize org csharp-mode cider clojure-mode slime markdown-mode)))
- '(safe-local-variable-values
-   (quote
-    ((cider-ns-refresh-after-fn . "integrant.repl/resume")
-     (cider-ns-refresh-before-fn . "integrant.repl/suspend")
-     (cider-refresh-after-fn . "integrant.repl/resume")
-     (cider-refresh-before-fn . "integrant.repl/suspend"))))
- '(tab-stop-list (quote (2 4 6 8 10 12 14 16 18 20))))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -66,8 +49,8 @@
 (load "org-config.el")
 (load "babel-config.el")
 (load "git-config.el")
+(load "ruby-config.el")
 ;(load "cl-config.el")
 ;(load "clojure-config.el")
-(load "ruby-config.el")
 
 
